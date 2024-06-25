8bc18215e628906f96ea83d813e09a644442f554
paul <paullevites84@gmail.com>
2024-06-25 19:09:10 +0100

    worked on functionalities

--- new.js
+++ new.js
@@ -3,15 +3,23 @@ const tip = document.querySelector(".tip");
 const total = document.querySelector(".total");
 const error = document.querySelector(".error");
 
+const hideError = ()=>{
+    setTimeout(() => {
+        error.style.display = "none"
+    }, 5000);
+}
+
 const calculateTip = ()=>{
     const bill = document.querySelector(".bill").value;
     const rate = document.querySelector(".rate").value;
 
     if (bill === "" || rate === ""){
         error.style.display = "block"
+        hideError()
     } else if (isNaN(bill)){
         error.innerHTML = "Please Enter a number";
         error.style.display = "block";
+        hideError()
     } else{
         let tipAmt = bill * rate
         tip.innerHTML = "Tip Amount: " + "$" + tipAmt
@@ -24,7 +32,6 @@ const calculateTip = ()=>{
 
 btn.addEventListener("click", calculateTip)
 
-// && - and
-// || - or
-// isNaN - is not a number
+
+
 
